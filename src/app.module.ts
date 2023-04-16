1535f37dc0d9a670f6ca54faba13ae752598f708
Piyal Darshana <pdarshana@bioseet.com>
2023-04-16 21:36:32 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LogsModule } from './logs/logs.module';
+
+
+@Module({
+  imports: [LogsModule, MongooseModule.forRoot('mongodb+srv://piyalcodes:0CAXatH6hZuK5lqC@store.4z9iw.mongodb.net/tracker')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
+
+
+
+
+
+ 
