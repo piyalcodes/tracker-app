d4ca2e022b0d656535bff012d55bde24acd5a043
Piyal Darshana <pdarshana@bioseet.com>
2023-05-01 17:06:21 +0530

    CORS

--- src/main.ts
+++ src/main.ts
@@ -18,25 +18,18 @@ async function bootstrap() {
   );
 */
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
+const options = {
+  "origin": true,
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "credentials":true
+}
+//app.use(cors(options))
+app.enableCors(options)
 
-  next();
-});
-
-  app.enableCors({
-    origin: "*",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-});
+ 
+ 
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
