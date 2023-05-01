dcbb7289787f50beb5f1d8dfd7c1242658e5c9c1
Piyal Darshana <pdarshana@bioseet.com>
2023-05-01 16:27:23 +0530

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const cors = require("cors")
+const cors = require('cors');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use(cors({    
-      "origin": "*",
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false,
-      "optionsSuccessStatus": 204    
-})); 
-
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -25,7 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
- 
   await app.listen(8080);
 }
 bootstrap();
